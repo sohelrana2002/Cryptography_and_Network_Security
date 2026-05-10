5182dc834c2cbc46d2f1c3cb7878f0e6541df620
SOHEL RANA <sohelrana104322@gmail.com>
2026-05-10 23:24:10 +0600

    random substitution table cipher

---
+++ Random_Substitution_Table/random_substitution_table.py
@@ -0,0 +1,43 @@
+# Random substitution 
+import random
+
+plain = "abcdefghijklmnopqrstuvwxyz"
+
+plain = "abcdefghijklmnopqrstuvwxyz"
+char_list = list(plain)
+random.shuffle(char_list) # random rearrange of list
+cipher = "".join(char_list)
+
+print("Random Cipher Table:", cipher)
+
+# encryption function 
+def encrypt(text):
+    result = ""
+
+    for ch in text.lower():
+        if ch in plain:
+            p_index = plain.index(ch)
+            result += cipher[p_index]
+        else:
+            result += ch
+    return result
+
+# decrypt function
+def decrypt(text):
+    result = ""
+
+    for ch in text:
+        if ch in cipher:
+            c_index = cipher.index(ch)
+            result += plain[c_index]
+        else:
+            result += ch
+    return result
+
+plain_text = input("Enter a message: ")
+cipher_text = encrypt(plain_text)
+decipher_text = decrypt(cipher_text)
+
+print("Orginal text: ", plain_text)
+print("Cipher text: ", cipher_text)
+print("Decipher text: ", decipher_text)
